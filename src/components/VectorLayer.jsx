dd43d1f5c06162e7620c0743d093da5bec9dec0e
Mastermind <adam.gicevic@gmail.com>
2023-05-11 21:56:43 +0200

    Some tests.

--- src/components/VectorLayer.jsx
+++ src/components/VectorLayer.jsx
@@ -8,23 +8,26 @@ import 'leaflet.vectorgrid';
 function VectorLayer({ name, url, vectorTileLayerStyles, ...props }) {
     const context = useLeafletContext();
 
-    const vectorTileLayerStylesIn = {
-        sliced: {
-            color: '#0000ff',
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.5,
-            fillColor: '#0000ff'
-        }
+
+    const vectorTileLayerStylesIn =
+    {
+        weight: 0.5,
+        opacity: 1,
+        color: '#ccc',
+        fillColor: '#390870',
+        fillOpacity: 0.6,
+        fill: true,
+        stroke: true
     }
+    
 
 
     useEffect(() => {
         if (context.map) {
             const vectorGrid = L.vectorGrid.protobuf(url, {
                 rendererFactory: L.canvas.tile,
-                vectorTileLayerStylesIn,
-                subdomains: "123",
+                vectorTileLayerStyles: vectorTileLayerStylesIn,
+                subdomains: "abcd",
             }).addTo(context.map);
 
             return () => {
@@ -37,49 +40,10 @@ function VectorLayer({ name, url, vectorTileLayerStyles, ...props }) {
         <TileLayer
             {...props}
             url={url}
+            attribution="222"
+            opacity={1}
         />
     );
 }
 
 export default VectorLayer;
-
-
-// import React, { useEffect, useRef } from 'react';
-// import L from 'leaflet';
-// import 'leaflet.vectorgrid';
-// import { MapContainer } from 'react-leaflet';
-
-// const LeafletLayer = (props) => {
-//     const { mapRef, children, ...rest } = props;
-//     return (
-//         <MapContainer {...rest} ref={mapRef}>
-//             {children}
-//         </MapContainer>
-//     );
-// };
-
-// function VectorLayer({ name, url, vectorTileLayerStyles, ...props }) {
-//     const mapRef = useRef(null);
-
-//     console.log(mapRef);
-//     useEffect(() => {
-//         if (mapRef.current && mapRef.current.leafletElement) {
-//             const vectorGrid = L.vectorGrid.protobuf(url, {
-//                 rendererFactory: L.canvas.tile,
-//                 vectorTileLayerStyles,
-//             }).addTo(mapRef.current.leafletElement);
-
-//             return () => {
-//                 mapRef.current.leafletElement.removeLayer(vectorGrid);
-//             };
-//         }
-//     }, [url, vectorTileLayerStyles]);
-
-//     return (
-//         <LeafletLayer mapRef={mapRef}>
-//             <div ref={mapRef} />
-//         </LeafletLayer>
-//     );
-// }
-
-// export default VectorLayer;
