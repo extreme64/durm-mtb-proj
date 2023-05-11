c2e07fbd65fdce69b2d5d3e33d4bd5eaa363fc60
Mastermind <adam.gicevic@gmail.com>
2023-05-11 21:52:32 +0200

    BarChart comments removed.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
-// import BarChart from './BarChart';
 import TrailMap from './components/TrailMap';
 
 function App() {
 
- 
-  
   return (
     <div className="App">
       <header></header>
       <main>
         <TrailMap />
-        {/* <BarChart data={data} /> */}
       </main>
     </div>
   );
