c52964d74557f1a3e142fdb49045f9c0f5c1369e
Mastermind <adam.gicevic@gmail.com>
2023-05-11 22:00:01 +0200

    Custom react comp to wrap vectorGrid.

--- src/components/VectorLayer.jsx
+++ src/components/VectorLayer.jsx
@@ -1,49 +1,50 @@
-import React, { useEffect, useRef } from 'react';
+// FIXME: remove or vault it
 
-import { MapContainer, TileLayer } from 'react-leaflet';
-import { useLeafletContext } from '@react-leaflet/core'
-import L from 'leaflet';
-import 'leaflet.vectorgrid';
+// import React, { useEffect, useRef } from 'react';
 
-function VectorLayer({ name, url, vectorTileLayerStyles, ...props }) {
-    const context = useLeafletContext();
+// import { MapContainer, TileLayer } from 'react-leaflet';
+// import { useLeafletContext } from '@react-leaflet/core'
+// import L from 'leaflet';
+// import 'leaflet.vectorgrid';
+
+// function VectorLayer({ name, url, vectorTileLayerStyles, ...props }) {
+//     const context = useLeafletContext();
 
 
-    const vectorTileLayerStylesIn =
-    {
-        weight: 0.5,
-        opacity: 1,
-        color: '#ccc',
-        fillColor: '#390870',
-        fillOpacity: 0.6,
-        fill: true,
-        stroke: true
-    }
+//     const vectorTileLayerStylesIn =
+//     {
+//         weight: 0.5,
+//         opacity: 1,
+//         color: '#ccc',
+//         fillColor: '#390870',
+//         fillOpacity: 0.6,
+//         fill: true,
+//         stroke: true
+//     }
     
 
+//     useEffect(() => {
+//         if (context.map) {
+//             const vectorGrid = L.vectorGrid.protobuf(url, {
+//                 rendererFactory: L.canvas.tile,
+//                 vectorTileLayerStyles: vectorTileLayerStylesIn,
+//                 subdomains: "abcd",
+//             }).addTo(context.map);
 
-    useEffect(() => {
-        if (context.map) {
-            const vectorGrid = L.vectorGrid.protobuf(url, {
-                rendererFactory: L.canvas.tile,
-                vectorTileLayerStyles: vectorTileLayerStylesIn,
-                subdomains: "abcd",
-            }).addTo(context.map);
+//             return () => {
+//                 context.map.removeLayer(vectorGrid);
+//             };
+//         }
+//     }, [context.map]);
 
-            return () => {
-                context.map.removeLayer(vectorGrid);
-            };
-        }
-    }, [context.map]);
+//     return (
+//         <TileLayer
+//             {...props}
+//             url={url}
+//             attribution="222"
+//             opacity={1}
+//         />
+//     );
+// }
 
-    return (
-        <TileLayer
-            {...props}
-            url={url}
-            attribution="222"
-            opacity={1}
-        />
-    );
-}
-
-export default VectorLayer;
+// export default VectorLayer;
